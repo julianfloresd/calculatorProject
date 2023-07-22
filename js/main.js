9a71b1ec437bf6c05bc58e20829a1252e8210b18
Julianfloresd <106127778+julianfloresd@users.noreply.github.com>
2023-07-22 12:32:33 -0400

    Project finished

--- js/main.js
+++ js/main.js
@@ -1,31 +1,135 @@
-const findTheOldest = function(array) {
-  const date = new Date()
-  let currentYear = date.getFullYear
-  
-  array.forEach(element => {
-      if (element.hasOwnProperty("yearOfDeath") == true) {
-        element.ageCalc = element.yearOfDeath - element.yearOfBirth
-      } else {
-        element.ageCalc = currentYear - element.yearOfBirth
-      }
-    
-  });
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const equalBtn = document.querySelector('#equal')
+const clearBtn = document.querySelector('#reset')
+const floatBtn = document.querySelector('.float')
+const backSpace = document.querySelector('.backSpace')
+let previousInput = document.querySelector('.previousInput')
+let currentInput = document.querySelector('.currentInput')
+let previousNumber = "";
+let currentNumber = "";
+let operator = "";
 
-  let highestAge = 0;
+numbers.forEach(button => button.addEventListener('click', function(e){
+  registerNumbers(e);
+  currentInput.textContent = currentNumber;
+}))
 
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i].ageCalc > highestAge) {
-      highestAge = array[i].ageCalc
-    } else {
-      continue;
-    }
+operators.forEach(button => button.addEventListener('click', function(e){
+  if (operator === "") {
+    registerOperation(e);
+  } else {
+    operate(operator, previousNumber, currentNumber)
+    previousNumber = ans;
+    registerOperation(e);
   }
+}))
 
-  for (let n = 0; n < array.length - 1; n++) {
-    if (array[n].ageCalc == highestAge) {
-      return array[n]
-    } else {
-      continue;
-    }
+equalBtn.addEventListener('click', () => {
+  operate(operator, previousNumber, currentNumber)
+  currentInput.textContent = ans;
+  previousNumber = ans;
+  previousInput.textContent = "";
+  currentNumber = ""
+  operator = "";
+})
+
+backSpace.addEventListener('click', () => {
+  deleteNumber ();
+  currentInput.textContent = currentNumber;
+})
+
+clearBtn.addEventListener('click', () => {
+  currentInput.textContent = "";
+  previousInput.textContent = "";
+  currentNumber = "";
+  previousNumber = "";
+  operator = "";
+})
+
+floatBtn.addEventListener('click', function(e) {
+  floatInput(e);
+})
+
+
+function registerNumbers(e) {
+  if (currentInput.textContent.length < 8){
+    currentNumber += e.target.textContent;
   }
+}
+
+function registerOperation(e) {
+  if (currentInput.textContent == "") {
+    currentInput.textContent = 0;
+  }
+  if (previousInput.textContent == 0 && currentInput.textContent.includes('.') == 0) {
+    previousNumber = parseInt(currentInput.textContent);
+  } else if (previousInput.textContent == 0 && currentInput.textContent.includes('.')){
+    previousNumber = parseFloat(currentInput.textContent)
+  }
+  operator = e.target.textContent;
+  previousInput.textContent = `${previousNumber} ${operator} `
+  currentNumber = "";
+  currentInput.textContent = currentNumber;
+}
+
+
+function floatInput(e) {
+  if(currentInput.textContent.includes('.')) {
+    currentInput.textContent = currentInput.textContent;
+  } else {
+    currentNumber += e.target.textContent;
+    currentInput.textContent = currentNumber;
+  }
+}
+
+
+function deleteNumber (){
+  currentNumber = currentNumber.slice(0, -1);
+}
+
+
+function operate (operator, previousNumber, currentNumber) {
+  if (currentInput.textContent == ""){
+    currentInput.textContent = 0;
+  }
+  if (currentInput.textContent.includes('.')) {
+    currentNumber = parseFloat(currentInput.textContent);
+  } else {
+    currentNumber = parseInt(currentInput.textContent);
+  }  
+  if (operator == '+') {
+    ans = add(previousNumber, currentNumber);
+    return ans;
+  } else if (operator == '-') {
+    ans = subtract(previousNumber, currentNumber);
+    return ans;
+  } else if (operator == '-') {
+    ans = multiply(previousNumber, currentNumber);
+    return ans;
+  } else if (operator == '-') {
+    ans = divide(previousNumber, currentNumber);
+    return ans;
+  } else {
+    ans = currentNumber
+  }
+}
+
+
+
+
+const add = (numOne, numTwo) => {
+  return numOne + numTwo
+};
+
+const subtract = (numOne, numTwo) => {
+  return numOne - numTwo
+};
+
+const multiply = (numOne, numTwo) => {
+  return numOne * numTwo
+};
+
+const divide = (numOne, numTwo) => {
+  return numOne / numTwo
 };
